a4a8f08f4b819c96ff41678a95291aa6d2924e10
ata oz <atariser123@gmail.com>
2018-04-04 16:28:21 +0300

    lots of

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+#look at the models file we have on this folder
+from .models import Job
+
+
+
+
 # Create your views here.
+
+def home(request):
+    jobs = Job.objects
+    return render(request, 'jobs/home.html', {'jobs': jobs})
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-
+import jobs.views
 
 urlpatterns = [
+    path('',jobs.views.home, name='home'),
     path('admin/', admin.site.urls),
+
 ]   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

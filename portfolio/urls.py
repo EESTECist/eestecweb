2d851abf429857261d1fa7b590ae36ffb3ec22d7
ata oz <atariser123@gmail.com>
2018-04-04 22:27:24 +0300

    lots of changes

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-import jobs.views
+import home.views
 
 urlpatterns = [
-    path('',jobs.views.home, name='home'),
+    path('',home.views.home, name='home'),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
 

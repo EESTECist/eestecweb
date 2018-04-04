5a5f0c3aaeb0845b656a9ef46cae0fccc46804b1
mehmetaltuner <altuner17@itu.edu.tr>
2018-04-04 21:36:26 +0300

    Database configurations

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -77,6 +77,10 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'postgres': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',

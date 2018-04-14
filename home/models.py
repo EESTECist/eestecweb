733f310927f9c7b7511f0c9cdfa3ece2ef89456a
ata oz <atariser123@gmail.com>
2018-04-14 18:17:06 +0300

    home app ine post modeli ekledim

--- home/models.py
+++ home/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    text = models.TextField()
+    pub_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __unicode__(self):
+        return self.title
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('blog_post_detail', (),
+                {
+                    'slug' :self.slug,
+                })

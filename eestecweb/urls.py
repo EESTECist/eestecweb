7ff9b18d2dc9f714b8cb79b5b7c2700438f7aaed
hickedby <tufekcih16@itu.edu.tr>
2018-04-17 23:34:49 +0300

    update

--- eestecweb/urls.py
+++ eestecweb/urls.py
@@ -10,4 +10,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls', namespace='user')),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
